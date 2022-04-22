b5ec831772d7ae82049ef110349211619d462ab5
Shubhansh Jain <93034177+ShubhanshJain@users.noreply.github.com>
2022-04-22 16:12:09 -0700

    Add files via upload

---
+++ Hardware_info.py
@@ -0,0 +1,11 @@
+# Qucik program to get python information using python
+
+import platform
+
+print(f"Computer network name: {platform.node()}")
+print(f"Machine type: {platform.machine()}")
+print(f"Processor type: {platform.processor()}")
+print(f"Platform type: {platform.platform()}")
+print(f"Operating system: {platform.system()}")
+print(f"Operating system release: {platform.release()}")
+print(f"Operating system version: {platform.version()}")
